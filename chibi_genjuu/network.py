a380b7f6268c204a7e941864ee54ac8ff23e65f6
dem4ply <dem4ply@gmail.com>
2019-06-13 17:17:45 -0500

    se agregaron las capas densas

--- chibi_genjuu/network.py
+++ chibi_genjuu/network.py
@@ -3,6 +3,8 @@ import yaml
 import lasagne
 from lasagne import layers
 from lasagne import objectives
+from chibi.module import export, import_
+from chibi.file import Chibi_path
 
 import theano
 import theano.tensor as T
@@ -10,6 +12,12 @@ import theano.tensor as T
 import numpy as np
 
 __all__ = [ 'Network' ]
+
+
+def get_function( name ):
+    name = name.lower()
+    if name == 'softmax':
+        return lasagne.nonlinearities.softmax
 
 
 class Layer( Chibi_object ):
@@ -20,11 +28,15 @@ class Layer( Chibi_object ):
 
     @property
     def dict( self ):
-        return dict( name=self.name )
+        return dict( name=self.name, type=export( self ) )
 
     @property
     def yaml( self ):
         return yaml.dump( self.dict )
+
+    @classmethod
+    def from_dict( cls, d ):
+        return cls( **d )
 
 
 class Input( Layer ):
@@ -68,9 +80,30 @@ class Input( Layer ):
             input_var=self.type_input_var) )
         return result
 
-    @classmethod
-    def from_dict( cls, d ):
-        return cls( **d )
+
+class Dense( Layer ):
+    input = descriptor.String()
+    function_no_liniarity = descriptor.String_choice(
+        default=None, choice=( 'softmax', '', None ) )
+    number_units = descriptor.Integer()
+
+    def __init__( self, *args, **kw ):
+        super().__init__( *args, **kw )
+        self._function_no_liniarity = get_function(
+            self.function_no_liniarity )
+
+    @property
+    def real_layer( self ):
+        return layers.DenseLayer( self._input, num_units=self.number_units )
+
+    @Layer.dict.getter
+    def dict( self ):
+        result = super().dict
+        result.update( dict(
+            input=self.input,
+            function_no_liniarity=self.function_no_liniarity,
+            number_units=self.number_units ) )
+        return result
 
 
 class Network( Chibi_object ):
@@ -82,3 +115,57 @@ class Network( Chibi_object ):
     name: string
     """
     name = descriptor.String( default='test_network' )
+    layers = descriptor.Dict()
+    meta_layers = descriptor.Dict()
+    order_layers = descriptor.List_kind_strict( kind=str )
+    functions = descriptor.Dict()
+
+    def __init__( self, layers=None, meta_layers=None, order_layers=None,
+                  *args, **kargs ):
+        if layers is None:
+            layers = {}
+        if meta_layers is None:
+            meta_layers = {}
+        if order_layers is None:
+            order_layers = []
+        super().__init__( layers=layers, meta_layers=meta_layers,
+                          order_layers=order_layers, functions={},
+                          *args, **kargs )
+
+    def add_layer( self, layer ):
+        self.layers[ layer.name ] = layer
+        self.order_layers.append( layer.name )
+
+    def save( self, path=None, folder='.' ):
+        if path is None:
+            path = self.name
+
+        path = Chibi_path( path )
+        if path.is_a_folder:
+            path += self.name
+        path = path.add_extensions( 'chibi', 'neuronal', 'yml' )
+
+        layers = { name: layer.dict for name, layer in self.layers.items() }
+
+        file_content = {
+            'layers': layers,
+            'order_layers': self.order_layers,
+            'name': self.name,
+        }
+
+        path.open().write_yaml( file_content )
+        return path
+
+        mad_files.write_json( full_rute, file_content )
+
+    @classmethod
+    def load( cls, path ):
+        path = Chibi_path( path )
+        self = cls()
+        read = path.open().read_yaml()
+        for name, dict_layers in read[ 'layers' ].items():
+            layer_type = import_( dict_layers[ 'type' ] )
+            self.layers[ name ] = layer_type.from_dict( dict_layers )
+        self.name = read[ 'name' ]
+        self.order_layers = read[ 'order_layers' ]
+        return self
--- tests/network.py
+++ tests/network.py
@@ -1,10 +1,23 @@
 from unittest import TestCase
+import tempfile, shutil
 from chibi.module import export
 from chibi_genjuu.index import Population, Dweller, Sample
 from elasticsearch_dsl import Document, field
+import lasagne
 
 
-from chibi_genjuu.network import Input, Network, Layer
+from chibi_genjuu.network import Input, Network, Layer, Dense
+
+class Test_with_folder( TestCase ):
+    def setUp(self):
+        super().setUp()
+        self.root_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree( self.root_dir )
+        return super().tearDown()
+
+
 
 
 class Test_layer( TestCase ):
@@ -33,4 +46,106 @@ class Test_layer_input( Test_layer ):
     def test_from_dict( self ):
         d = self.layer.dict
         result_layer = Input.from_dict( d )
-        self.assertEqual( result_layer.dict == self.layer.dict )
+        self.assertEqual( result_layer.dict, self.layer.dict )
+
+
+class Test_layer_dense( Test_layer ):
+    def setUp( self ):
+        super().setUp()
+        self.layer = Dense( name="dense", input='input' )
+
+    def test_from_dict( self ):
+        d = self.layer.dict
+        result_layer = Dense.from_dict( d )
+        self.assertEqual( result_layer.dict, self.layer.dict )
+
+    def test_if_function_no_liniarity_is_none_should_be_create( self ):
+        self.assertIsNone( self.layer._function_no_liniarity )
+
+    def test_if_function_no_liniarity_is_none_should_be_create( self ):
+        self.layer = Dense(
+            name="dense", input='input',
+            function_no_liniarity='softmax' )
+
+        self.assertEqual(
+            lasagne.nonlinearities.softmax,
+            self.layer._function_no_liniarity )
+
+
+class Test_network( Test_with_folder ):
+    def setUp( self ):
+        super().setUp()
+        self.net = Network( name=self._testMethodName )
+
+    def test_init( self ):
+        self.assertEqual( self.net.name, "test_init" )
+        self.assertIsInstance( self.net.layers, dict )
+        self.assertEqual( self.net.layers, dict() )
+        self.assertIsInstance( self.net.meta_layers, dict )
+        self.assertEqual( self.net.meta_layers, dict() )
+        self.assertIsInstance( self.net.functions, dict )
+        self.assertEqual( self.net.functions, dict() )
+        self.assertIsInstance( self.net.order_layers, list )
+        self.assertEqual( self.net.order_layers, list() )
+
+    def test_add_layer_input( self ):
+        self.assertFalse( self.net.layers, "la red tiene capas" )
+        layer = Input( name="input" )
+        self.net.add_layer( layer )
+        self.assertEqual( layer, self.net.layers[ layer.name ] )
+
+    def test_save( self ):
+        self.assertFalse( self.net.layers, "la red tiene capas" )
+        layer = Input( name="input" )
+        self.net.add_layer( layer )
+        path = self.net.save( self.root_dir )
+        result = path.open().read_yaml()
+        expected = {
+            'name': 'test_save',
+            'order_layers': [ 'input' ],
+            'layers': { 'input': {
+                'batch_size': None,
+                'input_var': 'int64',
+                'name': 'input',
+                'name': 'input',
+                'shape': [ None ],
+                'type': 'chibi_genjuu.network.Input',
+            } }
+        }
+        self.assertEqual( expected, result )
+
+
+class Test_network_save_load( Test_with_folder ):
+    def setUp( self ):
+        super().setUp()
+        self.net = Network( name=self._testMethodName )
+        self.assertFalse( self.net.layers, "la red tiene capas" )
+        self.layer = Input( name="input" )
+        self.net.add_layer( self.layer )
+
+    def test_save( self ):
+        path = self.net.save( self.root_dir )
+        result = path.open().read_yaml()
+        expected = {
+            'name': 'test_save',
+            'order_layers': [ 'input' ],
+            'layers': { 'input': {
+                'batch_size': None,
+                'input_var': 'int64',
+                'name': 'input',
+                'name': 'input',
+                'shape': [ None ],
+                'type': 'chibi_genjuu.network.Input',
+            } }
+        }
+        self.assertEqual( expected, result )
+
+    def test_load( self ):
+        path = self.net.save( self.root_dir )
+        new_network = Network.load( path )
+        self.assertEqual( self.net.order_layers, new_network.order_layers )
+        self.assertEqual( self.net.name, new_network.name )
+        for layer_name in self.net.order_layers:
+            self.assertEqual(
+                self.net.layers[ layer_name ].dict,
+                new_network.layers[ layer_name ].dict, )
